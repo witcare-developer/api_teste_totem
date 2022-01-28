afd68b2bf2fc056ca97d9534ef5732747397ad3a
witcare-developer <renato@witcare.com.br>
2022-01-28 15:27:48 -0300

    Funcao para bixar arquivos

--- python/baixa_arquivo.py
+++ python/baixa_arquivo.py
@@ -1,11 +1,34 @@
+import json
 import requests
 
-url = 'http://niblle.kinghost.net/image/'
+# 
+# 
+# 
+# 
+# 
+# 
+
+# print("download successful")
+
+url_json = 'http://niblle.kinghost.net/banco.php?path=produtos'
+url_imagem = 'http://niblle.kinghost.net/image/'
 diretorio = 'image/'
 imagem = "logo.png"
-f = open( diretorio + imagem ,'wb')
-response = requests.get(url + imagem )
-f.write(response.content)
-f.close()
 
-print("download successful")
+response = requests.get(url_json)
+array = response.content.decode('utf-8')
+array = json.loads(array)
+
+for dado in array:
+    print(str(dado['id']))
+    print(dado['nome_produto'])
+    print(dado['valor_produto'])
+    print(dado['link_imagem']+"\n")
+
+
+# print(array[1]['link_imagem'])
+
+# f = open( diretorio + imagem ,'wb')
+# response = requests.get(url_imagem + imagem )
+# f.write(response.content)
+# f.close()
